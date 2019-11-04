30052db1d4d74101735d524c5633af63fce79c92
Nik <nikvaes@gmail.com>
2019-11-04 01:46:04 +0100

    add flip logging and adaptivity rate decay

--- research_seed/cifar/cifar_trainer.py
+++ research_seed/cifar/cifar_trainer.py
@@ -1,7 +1,10 @@
 """
 This file runs the main training/val loop, etc... using Lightning Trainer"""
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks import ModelCheckpoint
+
 from argparse import ArgumentParser
+
 from cifar import BnnOnCIFAR10
 
 
@@ -9,12 +12,27 @@ def main(hparams):
     # init module
     model = BnnOnCIFAR10(hparams)
 
+    # DEFAULTS used by the Trainer
+    checkpoint_callback = ModelCheckpoint(
+        filepath="../../results",
+        save_best_only=True,
+        verbose=True,
+        monitor="val_loss",
+        mode="min",
+        prefix="",
+    )
+
     # most basic trainer, uses good defaults
     trainer = Trainer(
         max_nb_epochs=hparams.max_nb_epochs,
         gpus=hparams.gpus,
-        nb_gpu_nodes=hparams.nodes
+        nb_gpu_nodes=hparams.nodes,
+        overfit_pct=0.001,
+        # checkpoint_callback=checkpoint_callback
+        default_save_path="../../results",
+        check_val_every_n_epoch=25
     )
+
     trainer.fit(model)
 
 
@@ -22,7 +40,7 @@ if __name__ == "__main__":
     parser = ArgumentParser(add_help=False)
     parser.add_argument("--gpus", type=str, default=1)
     parser.add_argument("--nodes", type=int, default=1)
-    parser.add_argument("--max_nb_epochs", default=100, type=int)
+    parser.add_argument("--max_nb_epochs", default=500, type=int)
 
     # give the module a chance to add own params
     # good practice to define LightningModule specific params in the module
