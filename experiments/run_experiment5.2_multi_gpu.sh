23536abce98e1ea9f75ba6ef128bfa0af04579e6
Nik <nikvaes@gmail.com>
2019-11-27 23:36:20 +0100

    script for 2 GPUs

---
+++ experiments/run_experiment5.2_multi_gpu.sh
@@ -0,0 +1,16 @@
+DEBUG_RUN=0 # 1=True, 0=False
+
+python3.6 ../research_seed/cifar/cifar_trainer.py \
+  --gpus 2 \
+  --nodes 1 \
+  --max_nb_epochs 500 \
+  --debug $DEBUG_RUN \
+  --adaptivity-rate 10e-4 \
+  --threshold 10e-8 \
+  --batch_size 50 \
+  --adam-lr 0.01 \
+  --decay-n-epochs 100 \
+  --decay-exponential 0.1 \
+  --train-val-split 0.9
+
+sudo shutdown now
