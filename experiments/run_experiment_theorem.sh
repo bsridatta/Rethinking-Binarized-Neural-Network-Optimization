06fd01af999dbbceaaf2652bec48ea87ad5e8756
Sri Datta <b.sridatta@gmail.com>
2019-11-29 15:29:31 +0100

    fix epochs training script

--- experiments/run_experiment_theorem.sh
+++ experiments/run_experiment_theorem.sh
@@ -1,30 +1,30 @@
 python3 ../bytorch/experiments/with_larq/claims/larq_theorem_cifar.py \
   --lr 0.01 \
-  --epochs 1 \
+  --epochs 100 \
   --optim SGD \
   --init glorot_uniform
 
 python3 ../bytorch/experiments/with_larq/claims/larq_theorem_cifar.py \
   --lr 1 \
-  --epochs 1 \
+  --epochs 100 \
   --optim SGD \
   --init glorot_uniform
 
 python3 ../bytorch/experiments/with_larq/claims/larq_theorem_cifar.py \
   --lr 0.01 \
-  --epochs 1 \
+  --epochs 100 \
   --optim SGD \
   --init scaled_glorot_uniform
 
 python3 ../bytorch/experiments/with_larq/claims/larq_theorem_cifar.py \
   --lr 0.01 \
-  --epochs 1 \
+  --epochs 100 \
   --optim SGD \
   --init random_uniform
 
 python3 ../bytorch/experiments/with_larq/claims/larq_theorem_cifar.py \
   --lr 0.01 \
-  --epochs 1 \
+  --epochs 100 \
   --optim Adam \
   --init random_uniform
 
